9751b259651e9932dff216306f04c1052507a541
diaztawa <diaztawa3@gmail.com>
2020-04-29 16:51:21 +0800

    Hasil Video Part-3

--- Video Part-1/materi/javascript/script.js
+++ Video Part-1/materi/javascript/script.js
@@ -1,12 +1,13 @@
 (function (){
-    "use_strict";
+    "use strict";
+
     document.addEventListener("DOMContentLoaded", function(){
         var state = document.getElementById("s-state");
 
         document
-            .getElementById("cart-hplus")
-            .addEventListener("submit", estimateTotal);
-        
+        .getElementById("cart-hplus")
+        .addEventListener("submit", estimateTotal);
+
         var btnEstimate = document.getElementById("btn-estimate");
 
         btnEstimate.disabled = true;
@@ -14,7 +15,6 @@
         state.addEventListener("change", function(){
             if(state.value === ""){
                 btnEstimate.disabled = true;
-
             }else{
                 btnEstimate.disabled = false;
             }
@@ -22,7 +22,50 @@
 
         function estimateTotal(event){
             event.preventDefault();
-            alert("testing");
+            if(state.value === ""){
+                alert("Silakan pilih negara pengiriman");
+
+                state.focus();
+                return;
+            }
+
+        var itemBball = document.getElementById("txt-q-bball").value,
+        itemJersey = document.getElementById("txt-q-jersey").value,
+        itemPower = document.getElementById("txt-q-power").value,
+        shippingState = state.value,
+        shippingMethod = document.querySelector("[name=r_method]:checked").value;
+
+        var totalQty = itemBball + itemJersey + itemPower, 
+        shippingCostPer, shippingCost, taxFactor = 1, 
+        estimate, totalItemPrice = 90 * itemBball + 25 * itemJersey + 30 * itemPower;
+
+        if(shippingState === "CA"){
+            taxFactor = 1.075;
         }
+
+        switch (shippingMethod) {
+            case "usps":
+                shippingCostPer = 2;
+                
+                break;
+            
+            case "ups":
+                shippingCostPer = 3;
+                
+                break;
+        
+            default:
+                shippingCostPer = 0;
+                break;
+        }
+
+        shippingCost = shippingCostPer = totalQty;
+
+        estimate = totalItemPrice * taxFactor + shippingCost;
+
+        document.getElementById("txt-estimate").value = estimate;
+
+        }
+
     });
 })();
